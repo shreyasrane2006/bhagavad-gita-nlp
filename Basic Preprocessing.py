f9a479658aa6ad0799a71d2811825e247474fb9a
Shreyas1.Rane <shreyas1.rane@ril.com>
2025-04-11 15:46:17 +0530

    Day 4: Lemmatization and StopWords

--- Basic Preprocessing.py
+++ Basic Preprocessing.py
@@ -5,35 +5,45 @@ import collections
 from nltk import sent_tokenize
 from nltk import word_tokenize
 from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-
-verse = "Krishna said: O Arjuna, why are you hesitating in this sacred moment of battle?"
+#verse = "Krishna said: O Arjuna, why are you hesitating in this sacred moment of battle?"
 #sentence = sent_tokenize(verse)
 #print(sentence)
 
-words = word_tokenize(verse)
-print(words)
+#words = word_tokenize(verse)
+#print(words)
 
-cleaned_words = [word for word in words if word not in string.punctuation]
-print(cleaned_words)
+#cleaned_words = [word for word in words if word not in string.punctuation]
+#print(cleaned_words)
 
 
 with open("gita_intro.txt", "r") as file:
     lines = file.readlines()
     
 list_lines = [word for word in lines]
-#print(list_lines)
 
 s = ''.join(word for word in list_lines)
 
 words = word_tokenize(s)
-#print(words)
 
 extra_punct = {'--', '``', "''", "'s", "'t", '-', '“', '”', '’', '‘', '–'}
 cleaned_words = [word.lower() for word in words if word not in string.punctuation
                  and word not in extra_punct and word.isalpha()]
-print(cleaned_words)
+#print(cleaned_words)
 
 counts = Counter(cleaned_words)
-print(counts.most_common(10))
+#print(counts.most_common(10))
 
+stop_words = set(stopwords.words('english'))
+
+filtered_words = [word for word in cleaned_words if word not in stop_words]
+
+counts = Counter(filtered_words)
+#print(counts.most_common(10))
+
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
+
+print(lemmatized_words)
